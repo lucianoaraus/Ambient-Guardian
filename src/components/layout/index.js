7a7f6edecfe3c0b971019dbfc2fbe3aafe40efc2
lucianoaraus <lucianoaraus@gmail.com>
2022-06-30 16:28:13 -0300

    Modal component added

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Content from "./Content";
+import BasicModal from "./Modal";
 
 export default function Layout({ children, ...res }) {
   return (
@@ -8,6 +9,7 @@ export default function Layout({ children, ...res }) {
       <div {...res}>
         <Header />
         <Content />
+        <BasicModal />
         <div className="container ">{children}</div>
         <Footer />
       </div>
